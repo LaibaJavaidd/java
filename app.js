8677738ddc9cf02782e0a38dc35b64537e1a3384
LaibaaJavaid <2168405@brunel.ac.uk>
2022-11-24 13:28:35 +0000

    Final Commit

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require('express')
-const path = require('path')
 const stocks = require('./stocks')
+const path = require('path');
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+
+app.set('view engine', 'ejs')
+
+app.set('views', 'views')
+
+app.listen(3000, () => console.log('Server is running!'))
+
+//app.use(express.static(path.join(__dirname, 'static')))
+app.get('/', async (req, res) => {
+  res.render('index');
+})
 
 app.get('/stocks', async (req, res) => {
   const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+  res.render('stocks', {stockSym: stockSymbols});
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
   const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+  try{
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.render('stockPoints', {DATA: data})
+  }
+  catch(e){
+    console.log("ERRORs: ")
+    console.error(e)
+    res.render('timeout')
+  }
 })
 
-app.listen(3000, () => console.log('Server is running!'))
+app.use((req,res)=>{
+  res.render('404')
+})
